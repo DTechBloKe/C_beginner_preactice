3c7c5344b90363a7e9fb76befdc9ca58a7bd89df
DTechBloKe <ogunleyekolapo50@gmail.com>
2022-11-29 22:15:28 +0100

    unary operator

---
+++ unary_operator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	/*int a = 4, b = 3;
+	printf("%d", a+ + ++b);*/
+
+	int count = 10;
+
+	printf("%d\n", ++count);
+	printf("%d\n", ++count);
+	printf("%d\n", ++count);
+	return 0;
+}
